62531079e66ccf1381f4a4dac43d8ead6bbe4209
Bill Ma <billm1212@gmail.com>
2017-11-14 21:39:03 -0800

    initial

---
+++ directory_search.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import matplotlib.pyplot as plt
+
+
+def search_directory(root_dir, keyword):
+    '''performs dfs search on a directory'''
+    res = {}
+    for root, dirs, files in os.walk(root_dir):
+        count = 0
+        for f in files:
+            if keyword in f:
+                count += 1
+        res[root] = count
+
+    return res
+
+
+def barplot(d):
+    plt.bar(range(len(d)), d.values(), align="center")
+    plt.xticks(range(len(d)), list(d.keys()))
+
+    plt.show()
+
+
+def main():
+    data = search_directory(sys.argv[1], sys.argv[2])
+    print(data)
+    barplot(data)
+
+
+if __name__ == '__main__':
+    main()
